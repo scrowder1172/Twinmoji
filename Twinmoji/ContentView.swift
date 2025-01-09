5923a0dcaa7cc0db4ccc205b6dd0bd3f57318f28
Scott Crowder <scrowder@gmail.com>
2025-01-09 16:05:31 -0600

    Initial Commit

---
+++ Twinmoji/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// File: ContentView.swift
+// Project: Twinmoji
+// 
+// Created by SCOTT CROWDER on 1/9/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Twinmoji/TwinmojiApp.swift
@@ -0,0 +1,20 @@
+//
+// File: TwinmojiApp.swift
+// Project: Twinmoji
+// 
+// Created by SCOTT CROWDER on 1/9/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct TwinmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
