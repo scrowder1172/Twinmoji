5c536327cc98ddfc7498ea3202a5ad65ea71b07f
Scott Crowder <scrowder@gmail.com>
2025-01-09 18:12:20 -0600

    design menu

--- Twinmoji/MenuView.swift
+++ Twinmoji/MenuView.swift
@@ -17,7 +17,49 @@ struct MenuView: View {
     @State private var isGameActive: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isGameActive {
+            ContentView(itemCount: items, answerTime: timeOut, isGameActive: $isGameActive)
+        } else {
+            VStack(spacing: 10) {
+                Text("Twinmoji")
+                    .font(.largeTitle)
+                    .fontDesign(.rounded)
+                
+                Text("Answer Time")
+                    .font(.headline)
+                
+                Picker("Timeout", selection: $timeOut) {
+                    Text("Slow").tag(2.0)
+                    Text("Medium").tag(1.0)
+                    Text("Fast").tag(0.5)
+                }
+                .pickerStyle(.segmented)
+                .padding(.bottom)
+                
+                Text("Difficulty")
+                    .font(.headline)
+                
+                Picker("Difficulty", selection: $items) {
+                    Text("Easy").tag(9)
+                    Text("Hard").tag(12)
+                }
+                .pickerStyle(.segmented)
+                
+                Button("Start Game") {
+                    isGameActive = true
+                }
+                .buttonStyle(.borderedProminent)
+                
+                
+            }
+            .padding()
+            .background(.white)
+            .clipShape(.rect(cornerRadius: 20))
+            .shadow(radius: 10)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .background(Color(white: 0.9))
+        }
     }
 }
 
